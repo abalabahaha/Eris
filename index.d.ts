1010102fa3c401615b4e2c98c4901865038084a2
Ramsey <someramseyy@gmail.com>
2024-12-20 16:21:53 +0100

    Fix JSDoc and formatting

--- index.d.ts
+++ index.d.ts
@@ -507,10 +507,10 @@ declare namespace Eris {
     type: ApplicationRoleConnectionMetadataTypes;
   }
   interface ClientOptions {
-    application?: { id: string; flags?: number };
     /** @deprecated */
     agent?: HTTPSAgent;
     allowedMentions?: AllowedMentions;
+    application?: { id: string; flags?: number };
     autoreconnect?: boolean;
     compress?: boolean;
     connectionTimeout?: number;
